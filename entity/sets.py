6174581ab4c1c290c4243e9d9757370162d29d4f
eartser <eartser19@gmail.com>
2021-12-22 14:22:04 +0300

    add WrSIPP implementation

--- entity/sets.py
+++ entity/sets.py
@@ -52,3 +52,63 @@ class Closed:
 
     def was_expanded(self, item: Node):
         return item.get_pos() in self.nodes
+
+
+class OpenAndClosed:
+
+    def __init__(self):
+        self.data = []
+        self.nodes = {}
+        self.exp = {}
+        self.reexp = {}
+        self.reexp_cnt = 0
+
+    def __iter__(self):
+        return iter(self.nodes.values())
+
+    def __len__(self):
+        return len(self.nodes)
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def expand(self, node):
+        pos = node.get_pos()
+        if pos in self.exp:
+            self.reexp_cnt += 1
+            self.reexp[pos] = node
+        self.exp[pos] = node
+
+    def add_node(self, node):
+        pos = node.get_pos()
+        if pos in self.nodes:
+            cur_node = self.nodes[pos]
+            if cur_node.g <= node.g:
+                return
+            if cur_node in self.data:
+                self.data.remove(cur_node)
+        self.nodes[pos] = node
+        if pos in self.exp:
+            self.exp[pos] = node
+        if pos in self.reexp:
+            self.reexp[pos] = node
+        heapq.heappush(self.data, node)
+
+    def get_best_node(self):
+        best = self.data[0]
+        heapq.heappop(self.data)
+        self.expand(best)
+        return best
+
+    @property
+    def expanded(self):
+        return self.exp.values()
+
+    @property
+    def reexpanded(self):
+        return self.reexp.values()
+
+    @property
+    def number_of_reexpansions(self):
+        return self.reexp_cnt
+
--- search_functions/WSIPP.py
+++ search_functions/WSIPP.py
@@ -1,5 +1,5 @@
 from search_functions.SearchFunction import SearchFunction
-from entity.sets import Open, Closed
+from entity.sets import Open, Closed, OpenAndClosed
 from entity.Node import Node
 
 
@@ -32,4 +32,22 @@ class WdSIPP(SearchFunction):
 
 
 class WrSIPP(SearchFunction):
-    pass
+
+    def find(self, grid_map, start_i, start_j, goal_i, goal_j):
+        OPEN_AND_CLOSED = OpenAndClosed()
+
+        start = Node(start_i, start_j, h=self.heuristic_function(start_i, start_j, goal_i, goal_j))
+
+        OPEN_AND_CLOSED.add_node(start)
+
+        while not OPEN_AND_CLOSED.is_empty():
+            cur_node = OPEN_AND_CLOSED.get_best_node()
+            if cur_node.i == goal_i and cur_node.j == goal_j:
+                return True, cur_node, OPEN_AND_CLOSED
+
+            for i, j, interval, t in grid_map.get_successors(cur_node):
+                h = self.w * self.heuristic_function(i, j, goal_i, goal_j)
+                new_node = Node(i, j, g=t, h=h, interval=interval, parent=cur_node)
+                OPEN_AND_CLOSED.add_node(new_node)
+
+        return False, None, OPEN_AND_CLOSED
