789a71be09a3856e68356301a6a0cdfbf9180920
eartser <eartser19@gmail.com>
2021-12-23 20:28:13 +0300

    fix statistics

--- entity/Statistics.py
+++ entity/Statistics.py
@@ -1,12 +1,10 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class Statistics:
-    success_count = {}
-    runs_count = {}
-    sum_makespan = {}
-    sum_flowtime = {}
+
+    def __init__(self):
+        self.success_count = {}
+        self.runs_count = {}
+        self.sum_makespan = {}
+        self.sum_flowtime = {}
 
     def add_stat(self, cnt_agents, success, makespan, flowtime):
         if cnt_agents not in self.runs_count:
@@ -20,6 +18,15 @@ class Statistics:
             self.success_count[cnt_agents] += 1
             self.sum_makespan[cnt_agents] += makespan
             self.sum_flowtime[cnt_agents] += flowtime
+
+    def success_rate(self, agents_number):
+        return self.success_count[agents_number] / self.runs_count[agents_number]
+
+    def makespan(self, agents_number):
+        return self.sum_makespan[agents_number] / self.success_count[agents_number]
+
+    def flowtime(self, agents_number):
+        return self.sum_flowtime[agents_number] / self.success_count[agents_number]
 
     def __str__(self):
         s = ""
